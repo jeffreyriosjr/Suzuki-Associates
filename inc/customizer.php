832f34ad7219d5f8d132d3db17687b1c32322244
jeffrey rios jr <jeffriosjr@gmail.com>
2022-12-22 13:22:46 -0800

    created new template

---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Suzuki & Associates Theme Customizer
+ *
+ * @package Suzuki_&_Associates
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function suzuki_associates_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'suzuki_associates_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'suzuki_associates_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'suzuki_associates_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function suzuki_associates_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function suzuki_associates_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function suzuki_associates_customize_preview_js() {
+	wp_enqueue_script( 'suzuki-associates-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'suzuki_associates_customize_preview_js' );
