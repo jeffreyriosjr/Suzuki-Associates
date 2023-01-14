2efbc30d406f60fa9bee9c23423d2fcb2daeeeed
jeffrey rios jr <jeffriosjr@gmail.com>
2023-01-13 16:49:30 -0800

    homepage issues

--- front-page.php
+++ front-page.php
@@ -15,7 +15,7 @@ get_header();
 
 <main id="primary" class="site-main">
 <div class="container opening-page">
-<a class="btn" href="http://suzuki-associates.local/home" role="button"><img class="main-logo" src="<?php echo get_template_directory_uri(); ?>/images/main-logo.png"></a>
+<a class="btn" href="https://suzukiassociates.flywheelsites.com/home" role="button"><img class="main-logo" src="<?php echo get_template_directory_uri(); ?>/images/main-logo.png"></a>
 </div>
 </main><!-- #main -->
 
