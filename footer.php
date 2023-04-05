c1f3d406c733c707fea0a1cef03a6b663fe8eb00
jeffrey rios jr <jeffriosjr@gmail.com>
2023-04-04 17:46:06 -0700

    updated footer

--- footer.php
+++ footer.php
@@ -10,25 +10,99 @@
  */
 
 ?>
+<footer class="text-center text-lg-start bg-light text-muted">
+  <!-- Section: Social media -->
+  <section class="d-flex the-footer">
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'suzuki-associates' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'suzuki-associates' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'suzuki-associates' ), 'suzuki-associates', '<a href="http://underscores.me/">jeffrey alexander rios</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+    <div class="social-icons">
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="main-footer">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3 mb-3 text-center d-flex justify-content-center">
+        <div class="col-sm-3">
+          <!-- Links -->
+          <li class="nav-item">
+            <a href="http://suzuki-associates.local/services/" class="nav-link">Services</a>
+          </li>
+          <li class="nav-item">
+            <a href="http://suzuki-associates.local/clients/" class="nav-link">Clients</a>
+          </li>
+          <li class="nav-item">
+            <a href="http://suzuki-associates.local/about/" class="nav-link">About</a>
+          </li>
+          <li class="nav-item">
+            <a href="http://suzuki-associates.local/contact/" class="nav-link">Contact</a>
+          </li>
+        </div>
+        <!-- Grid column -->
+        <div class="col-sm-3 footer-logo">
+          <img class="nav-logo" src="http://suzuki-associates.local/wp-content/uploads/2023/03/11-e1679508581275.png"
+            alt="">
+        </div>
+        <!-- Grid column -->
+        <div class="col-sm-3">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            info@example.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2023 Copyright:
+    <a class="text-reset fw-bold" href="">Rios Productions</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- <footer id="colophon" class="site-footer">
+    <div class="site-info container-fluid">
+
+      <div class="footer-nav">
+      <ul class="nav-menu">
+
+            </ul>
+    </div>
+    </div><!site-info -->
+</footer>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'automattic/underscores',
+        'pretty_version' => 'dev-development',
+        'version' => 'dev-development',
+        'reference' => '30ce9e0800c48f5332c0f65f1d70b5aef10238fb',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'automattic/underscores' => array(
+            'pretty_version' => 'dev-development',
+            'version' => 'dev-development',
+            'reference' => '30ce9e0800c48f5332c0f65f1d70b5aef10238fb',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.7.2',
+            'version' => '3.7.2.0',
+            'reference' => 'ed8e00df0a83aa96acf703f8c2979ff33341f879',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
