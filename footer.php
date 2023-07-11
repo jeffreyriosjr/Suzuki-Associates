379ecbba3fb4fe5ab02ad9a760ce8a834008ac84
jeffrey rios jr <jeffriosjr@gmail.com>
2023-07-10 21:50:14 -0700

    final touceups to footer

--- footer.php
+++ footer.php
@@ -43,15 +43,15 @@
           </div>
           <!-- Grid column -->
 
-            <!-- Grid column -->
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mt-5">
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mt-5">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold">Contact</h6>
             <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
-            <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-            <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-            <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-            <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+            <p><i class="fas fa-home mr-3"></i> 8050 N Palm Ave Fresno, CA 93711</p>
+            <p><i class="fas fa-envelope mr-3"></i> Contact @ suzukiassociatesinc.com</p>
+            <p><i class="fas fa-phone mr-3"></i> +1-559-289-9801</p>
+            <p><i class="fas fa-print mr-3"></i> Suzuki & Associates, Inc.</p>
           </div>
           <!-- Grid column -->
 
@@ -61,16 +61,16 @@
             <h6 class="text-uppercase fw-bold">Resources</h6>
             <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
             <p>
-              <a href="#!" class="text-black">Your Account</a>
+              <a href="http://suzuki-associates.local/blog/" class="text-black">Latest Blogs</a>
             </p>
             <p>
-              <a href="#!" class="text-black">Become an Affiliate</a>
+              <a href="http://suzuki-associates.local/blog/" class="text-black">Latest Articles</a>
             </p>
             <p>
-              <a href="#!" class="text-black">Shipping Rates</a>
+              <a href="http://suzuki-associates.local/blog/" class="text-black">Hot Topics</a>
             </p>
             <p>
-              <a href="#!" class="text-black">Help</a>
+              <a href="http://suzuki-associates.local/blog/" class="text-black">Must Reads</a>
             </p>
           </div>
           <!-- Grid column -->
